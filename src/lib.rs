c40d7f4305e53b063bca8bb744dddaa034b90474
trulicups <livantgs@gmail.com>
2023-01-06 11:06:06 -0600

    cstr

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use wasmbus_rpc::actor::prelude::*;
 use wasmcloud_interface_httpclient::{
-    HttpClient, HttpClientSender, HttpRequest as HttpClientRequest,
+	HttpClient, HttpClientSender, HttpRequest as HttpClientRequest,
 };
 use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, HttpServerReceiver};
 use wasmcloud_interface_numbergen::random_in_range;
@@ -9,26 +9,26 @@ use wasmcloud_interface_numbergen::random_in_range;
 #[services(Actor, HttpServer)]
 struct XkcdgeneratorActor {}
 
-/// Implementation of HttpServer trait methods
+// Implementation of HttpServer trait methods
 #[async_trait]
 impl HttpServer for XkcdgeneratorActor {
-    async fn handle_request(&self, ctx: &Context, _req: &HttpRequest) -> RpcResult<HttpResponse> {
-        // Generate a comic number, between the first and most recent comic
-        let random_num = random_in_range(1, 2680).await?;
-        // Create request URL where XKCD stores JSON metadata about comics
-        let xkcd_url = format!("https://xkcd.com/{}/info.0.json", random_num);
-        let response = HttpClientSender::new()
-            .request(ctx, &HttpClientRequest::get(&xkcd_url))
-            .await?;
+	async fn handle_request(&self, ctx: &Context, _req: &HttpRequest) -> RpcResult<HttpResponse> {
+		// Generate a comic number, between the first and most recent comic
+		let random_num = random_in_range(1, 2680).await?;
+		// Create request URL where XKCD stores JSON metadata about comics
+		let xkcd_url = format!("https://xkcd.com/{}/info.0.json", random_num);
+		let response = HttpClientSender::new()
+			.request(ctx, &HttpClientRequest::get(&xkcd_url))
+			.await?;
 
-        // Deserialize JSON to retrieve comic title and img URL
-        let comic: XkcdComic = serde_json::from_slice(&response.body).map_err(|e| {
-            RpcError::ActorHandler(format!("Failed to deserialize comic request: {}", e))
-        })?;
+		// Deserialize JSON to retrieve comic title and img URL
+		let comic: XkcdComic = serde_json::from_slice(&response.body).map_err(|e| {
+			RpcError::ActorHandler(format!("Failed to deserialize comic request: {}", e))
+		})?;
 
-        // Format HTTP response body as an HTML string
-        let body = format!(
-            r#"
+		// Format HTTP response body as an HTML string
+		let body = format!(
+			r#"
         <!DOCTYPE html>
         <html>
         <head>
@@ -40,19 +40,19 @@ impl HttpServer for XkcdgeneratorActor {
         </body>
         </html>
             "#,
-            comic.title, comic.img
-        );
+			comic.title, comic.img
+		);
 
-        Ok(HttpResponse {
-            body: body.as_bytes().to_vec(),
-            ..Default::default()
-        })
-    }
+		Ok(HttpResponse {
+			body: body.as_bytes().to_vec(),
+			..Default::default()
+		})
+	}
 }
 
 #[derive(serde::Deserialize)]
 // XKCD comic metadata fields
 struct XkcdComic {
-    title: String,
-    img: String,
+	title: String,
+	img: String,
 }
